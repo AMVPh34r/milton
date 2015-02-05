4c8c6f4860ad7109b7770f5fa3cc9ec88edafa56
Alex Schaeffer <AMVPh34r@gmail.com>
2015-02-05 13:58:50 -0500

    Show benchmark in development environments

--- themes/default/views/_include/footer.php
+++ themes/default/views/_include/footer.php
@@ -1,9 +1,18 @@
 <footer class="bg-white">
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-10 col-lg-offset-1 text-center">
+			<div class="col-lg-8 col-lg-offset-2 text-center">
 				<p class="text-muted">Copyright &copy;2015 Archer Technologies.
 				Site design by Alex Schaeffer.</p>
+			</div>
+			<div class="col-lg-2 text-right">
+				<p class="text-muted">
+					<?php
+					if (in_array(ENVIRONMENT, array('development', 'testing'))) {
+						echo 'Page generated in ' . $this->benchmark->elapsed_time() . 's.';
+					}
+					?>
+				</p>
 			</div>
 		</div>
 	</div>
