ba716fb623c1b8ed8cb18d51a196f43936caec77
Joshua Crump <joshua@joshuacrump.com>
2015-02-11 20:40:13 -0500

    Took <br> out

--- themes/default/views/page.php
+++ themes/default/views/page.php
@@ -1,7 +1,6 @@
 <?php $this->load->view('_include/header'); ?>
 Sample page for testing controllers.<br />
 Page title: <strong><?php echo $this->sPageTitle; ?></strong>
-<br> Test
 <?php
 $this->load->view('_include/scripts');
 $this->load->view('_include/html_footer');
