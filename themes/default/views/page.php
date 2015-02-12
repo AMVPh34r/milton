b3e457dfe4a7c5673c81d435dd03611dcf183b94
Joshua Crump <joshua@joshuacrump.com>
2015-02-11 20:26:50 -0500

    Test of github

--- themes/default/views/page.php
+++ themes/default/views/page.php
@@ -1,6 +1,7 @@
 <?php $this->load->view('_include/header'); ?>
 Sample page for testing controllers.<br />
 Page title: <strong><?php echo $this->sPageTitle; ?></strong>
+<br> Test
 <?php
 $this->load->view('_include/scripts');
 $this->load->view('_include/html_footer');
